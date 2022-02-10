e6fb51c818645001ac1b78c509fb644bc2d389a0
Le Tran Dong <letrandong1199@gmail.com>
2022-02-11 00:40:04 +0700

    sql injection

---
+++ SQLInjectionDemo/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace SQLInjectionDemo
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txt_username = new System.Windows.Forms.Label();
+            this.txt_password = new System.Windows.Forms.Label();
+            this.txtbox_username = new System.Windows.Forms.TextBox();
+            this.txtbox_password = new System.Windows.Forms.TextBox();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.btn_cancel = new System.Windows.Forms.Button();
+            this.sqlinjectionDBDataSet = new SQLInjectionDemo.sqlinjectionDBDataSet();
+            this.usersBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.usersTableAdapter = new SQLInjectionDemo.sqlinjectionDBDataSetTableAdapters.UsersTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.sqlinjectionDBDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.usersBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_username
+            // 
+            this.txt_username.Font = new System.Drawing.Font("Times New Roman", 20F);
+            this.txt_username.Location = new System.Drawing.Point(87, 49);
+            this.txt_username.Name = "txt_username";
+            this.txt_username.Size = new System.Drawing.Size(158, 34);
+            this.txt_username.TabIndex = 0;
+            this.txt_username.Text = "Username";
+            // 
+            // txt_password
+            // 
+            this.txt_password.Font = new System.Drawing.Font("Times New Roman", 20F);
+            this.txt_password.Location = new System.Drawing.Point(87, 108);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.Size = new System.Drawing.Size(158, 34);
+            this.txt_password.TabIndex = 1;
+            this.txt_password.Text = "Password";
+            // 
+            // txtbox_username
+            // 
+            this.txtbox_username.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.usersBindingSource, "Username", true));
+            this.txtbox_username.Font = new System.Drawing.Font("Times New Roman", 16F);
+            this.txtbox_username.Location = new System.Drawing.Point(274, 51);
+            this.txtbox_username.Name = "txtbox_username";
+            this.txtbox_username.Size = new System.Drawing.Size(274, 32);
+            this.txtbox_username.TabIndex = 2;
+            // 
+            // txtbox_password
+            // 
+            this.txtbox_password.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.usersBindingSource, "Password", true));
+            this.txtbox_password.Font = new System.Drawing.Font("Times New Roman", 16F);
+            this.txtbox_password.Location = new System.Drawing.Point(274, 108);
+            this.txtbox_password.Name = "txtbox_password";
+            this.txtbox_password.Size = new System.Drawing.Size(274, 32);
+            this.txtbox_password.TabIndex = 3;
+            // 
+            // btn_login
+            // 
+            this.btn_login.Location = new System.Drawing.Point(274, 172);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(117, 37);
+            this.btn_login.TabIndex = 4;
+            this.btn_login.Text = "Login";
+            this.btn_login.UseVisualStyleBackColor = true;
+            this.btn_login.Click += new System.EventHandler(this.login_Click);
+            // 
+            // btn_cancel
+            // 
+            this.btn_cancel.Location = new System.Drawing.Point(431, 172);
+            this.btn_cancel.Name = "btn_cancel";
+            this.btn_cancel.Size = new System.Drawing.Size(117, 37);
+            this.btn_cancel.TabIndex = 5;
+            this.btn_cancel.Text = "Cancel";
+            this.btn_cancel.UseVisualStyleBackColor = true;
+            this.btn_cancel.Click += new System.EventHandler(this.cancel_Click);
+            // 
+            // sqlinjectionDBDataSet
+            // 
+            this.sqlinjectionDBDataSet.DataSetName = "sqlinjectionDBDataSet";
+            this.sqlinjectionDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // usersBindingSource
+            // 
+            this.usersBindingSource.DataMember = "Users";
+            this.usersBindingSource.DataSource = this.sqlinjectionDBDataSet;
+            // 
+            // usersTableAdapter
+            // 
+            this.usersTableAdapter.ClearBeforeFill = true;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 261);
+            this.Controls.Add(this.btn_cancel);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.txtbox_password);
+            this.Controls.Add(this.txtbox_username);
+            this.Controls.Add(this.txt_password);
+            this.Controls.Add(this.txt_username);
+            this.Name = "LoginForm";
+            this.Text = "Login Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.sqlinjectionDBDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.usersBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txt_username;
+        private System.Windows.Forms.Label txt_password;
+        private System.Windows.Forms.TextBox txtbox_username;
+        private System.Windows.Forms.TextBox txtbox_password;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Button btn_cancel;
+        private sqlinjectionDBDataSet sqlinjectionDBDataSet;
+        private System.Windows.Forms.BindingSource usersBindingSource;
+        private sqlinjectionDBDataSetTableAdapters.UsersTableAdapter usersTableAdapter;
+    }
+}
+
---
+++ SQLInjectionDemo/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQLInjectionDemo
+{
+    public partial class LoginForm : Form
+    {
+        SqlConnection con = new SqlConnection();
+        public LoginForm()
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "Data Source=DESKTOP-P85G4DJ\\SQLEXPRESS;Initial Catalog=sqlinjectionDB;Integrated Security=True";
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'sqlinjectionDBDataSet.Users' table. You can move, or remove it, as needed.
+
+            //this.usersTableAdapter.Fill(this.sqlinjectionDBDataSet.Users);
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-P85G4DJ\\SQLEXPRESS; Initial Catalog=sqlinjectionDB;Integrated Security = True");
+            con.Open();
+
+            {
+            }
+        }
+        private void login_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "Data Source=DESKTOP-P85G4DJ\\SQLEXPRESS; Initial Catalog=sqlinjectionDB;Integrated Security = True";
+            con.Open();
+            string username = txtbox_username.Text;
+            string password = txtbox_password.Text;
+            SqlCommand cmd = new SqlCommand("select Username,Password from Users where Username='" + username + "'and Password='" + password + "'", con);
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.HasRows)
+            {
+                MessageBox.Show("Login success Welcome to Homepage https://webmvc20220207162230.azurewebsites.net/");
+                System.Diagnostics.Process.Start("https://webmvc20220207162230.azurewebsites.net/");
+            }
+            else
+            {
+                MessageBox.Show("Invalid Login please check username and password");
+            }
+            con.Close();
+        }
+        private void cancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+
+        }
+
+
+    }
+
+        
+    
+}
